49c3d3ed6d60a80cc722d96ebcf2c4066c348f80
Souvik Banerjee <souvik1997@gmail.com>
2016-01-14 19:56:40 -0600

    Make terminal colors more readable

--- init.el
+++ init.el
@@ -26,7 +26,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(term-color-black ((t (:background "dark gray" :foreground "gray"))))
+ '(term-color-blue ((t (:background "deep sky blue" :foreground "cyan")))))
 
 
 
