5f05e602708ca4609d151462cb0b19b8db78a783
Souvik Banerjee <souvik1997@gmail.com>
2016-01-17 14:56:19 -0600

    Remove box around modeline

--- init.el
+++ init.el
@@ -41,6 +41,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(mode-line ((t (:background "#222226" :foreground "#b2b2b2" :box nil))))
+ '(mode-line-inactive ((t (:background "#292b2e" :foreground "#b2b2b2" :box nil))))
  '(term-color-black ((t (:background "dark gray" :foreground "gray"))))
  '(term-color-blue ((t (:background "deep sky blue" :foreground "cyan")))))
 
