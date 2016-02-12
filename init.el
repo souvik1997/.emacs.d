3905046bc038f0cf6d1911f1d615b9933c48db54
Souvik Banerjee <souvik1997@gmail.com>
2016-02-12 12:11:09 -0600

    add kill ring packages

--- init.el
+++ init.el
@@ -49,7 +49,7 @@
  '(org-support-shift-select (quote always))
  '(package-selected-packages
    (quote
-    (ein switch-window monokai-theme auctex-latexmk srefactor exec-path-from-shell elpy misc-cmds company auctex web-mode visible-mark spacemacs-theme spaceline smooth-scrolling smex projectile neotree markdown-mode magit js2-mode ido-yes-or-no ido-ubiquitous flycheck flx-ido dired+ company-web company-math company-auctex anzu)))
+    (browse-kill-ring+ easy-kill-extras easy-kill ein switch-window monokai-theme auctex-latexmk srefactor exec-path-from-shell elpy misc-cmds company auctex web-mode visible-mark spacemacs-theme spaceline smooth-scrolling smex projectile neotree markdown-mode magit js2-mode ido-yes-or-no ido-ubiquitous flycheck flx-ido dired+ company-web company-math company-auctex anzu)))
  '(powerline-default-separator (quote wave))
  '(powerline-height 23)
  '(projectile-keymap-prefix "p")
@@ -249,6 +249,14 @@
 ;; switch-window
 (require 'switch-window)
 (global-set-key (kbd "C-x o") 'switch-window)
+
+;; browse-kill-ring+
+(require 'browse-kill-ring)
+(browse-kill-ring-default-keybindings)
+
+;; easy-kill
+(require 'easy-kill)
+(global-set-key [remap kill-ring-save] 'easy-kill)
 
 ;; Miscellaneous configuration
 
