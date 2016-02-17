bd62fb1e555b3f25cb38420e2518145f2df1fb83
Souvik Banerjee <souvik1997@gmail.com>
2016-02-17 16:03:03 -0600

    Change copy-from-osx

--- init.el
+++ init.el
@@ -132,7 +132,13 @@
 
 ;; Hook up emacs clipboard with mac system clipboard
 (defun copy-from-osx ()
-  (shell-command-to-string "pbpaste"))
+  "Handle copy/paste intelligently on osx."
+  (let ((pbpaste (purecopy "/usr/bin/pbpaste")))
+    (if (and (eq system-type 'darwin)
+             (file-exists-p pbpaste))
+        (let ((tramp-mode nil)
+              (default-directory "~"))
+          (shell-command-to-string pbpaste)))))
 
 (defun paste-to-osx (text &optional push)
   (let ((process-connection-type nil))
